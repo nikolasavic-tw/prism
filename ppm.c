2c805a45e40122ac2c58f1955bc4ef584027ed18
Nikola Savic <nsavic@thoughtworks.com>
2023-05-06 16:27:12 -0400

    Refactor

--- ppm.c
+++ ppm.c
@@ -11,21 +11,17 @@ int main() {
   fprintf(f, "P6\n");
   fprintf(f, "%d %d 255\n", WIDTH, HEIGHT);
 
-  char r = 0;
-  char g = 0;
-  char b = 0;
-  int val;
-  float x = 0.0;
-  float y = 0.0;
+  char r, g, b;
+  float x, y;
 
   for(int i = 0; i < HEIGHT; i++) {
     for(int j = 0; j < WIDTH; j++) {
-      x = ( 1.0 * i)/ HEIGHT;
-      y = ( 1.0 * j)/ WIDTH;
+      y = ( 1.0 * i)/ HEIGHT;
+      x = ( 1.0 * j)/ WIDTH;
 
-      r = (int)((1-x) * 255.0);
-      b = (int)(x * 255.0);
-      g = (int)(y * 255.0);
+      r = (int)((1-y) * 255);
+      b = (int)(y * 255);
+      g = (int)(x * 255);
 
       fwrite(&r, sizeof(r), 1, f);
       fwrite(&g, sizeof(g), 1, f);
