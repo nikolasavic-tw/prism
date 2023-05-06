7ce397965dc19915268dd6541d2cf909affdd0a3
Nikola Savic <nsavic@thoughtworks.com>
2023-05-06 12:33:50 -0400

    Remove debug code

--- ppm.c
+++ ppm.c
@@ -8,8 +8,6 @@ int main() {
   FILE *f;
   f = fopen("rainbow.ppm", "wb");
 
-  char* h = "P6\n";
-  char* info = "16 16 255\n";
   fprintf(f, "P6\n");
   fprintf(f, "%d %d 255\n", WIDTH, HEIGHT);
 
@@ -22,7 +20,6 @@ int main() {
 
   for(int i = 0; i < HEIGHT; i++) {
     for(int j = 0; j < WIDTH; j++) {
-      val =  i * j;
       x = ( 1.0 * i)/ HEIGHT;
       y = ( 1.0 * j)/ WIDTH;
 
@@ -30,16 +27,11 @@ int main() {
       b = (int)(x * 255.0);
       g = (int)(y * 255.0);
 
-      if (0 && i % 500 == 0 && j % 500 ==0) {
-        printf("x: %f, y: %f--  r: %d b: %d g: %d", x, y, r, b, g);
-      }
-
       fwrite(&r, sizeof(r), 1, f);
       fwrite(&g, sizeof(g), 1, f);
       fwrite(&b, sizeof(b), 1, f);
     }
   }
-
 
   fclose(f);
 
